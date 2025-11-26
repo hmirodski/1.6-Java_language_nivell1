98b932d2bc27dc911a5aa05a05a570ebc6d0fd90
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-26 12:25:44 +0100

    feat tests for genericmethodstest

--- src/test/java/exercici2/GenericMethodsTest.java
+++ src/test/java/exercici2/GenericMethodsTest.java
@@ -9,5 +9,18 @@ import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GenericMethodsTest {
+    @Test
+    public void testWithPersonaStringDouble() {
+        GenericMethods gm = new GenericMethods();
+        Person p = new Person("Horacio", "Miroddi", 33);
+        assertDoesNotThrow(() -> gm.showGenericMethods(p, "Texto", 3.14));
+    }
+
+    @Test
+    public void testWithDifferentTypes() {
+        GenericMethods gm = new GenericMethods();
+        assertDoesNotThrow(() -> gm.showGenericMethods("Hola", 25, true));
+    }
+
 
 }
