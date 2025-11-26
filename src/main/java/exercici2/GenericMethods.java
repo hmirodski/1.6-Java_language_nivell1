f05af2ba37871501a11e3ba7b110f066fc243f91
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-26 12:05:28 +0100

    feat: GenericMethod class created

---
+++ src/main/java/exercici2/GenericMethods.java
@@ -0,0 +1,9 @@
+package exercici2;
+
+public class GenericMethods {
+    public <T, U, V> void shotGenericMethods(T first, U second, V third) {
+        System.out.println("1: " + first);
+        System.out.println("2: " + second);
+        System.out.println("3: " + third);
+    }
+}
