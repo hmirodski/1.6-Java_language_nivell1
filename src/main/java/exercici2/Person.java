2781d54b4358451191c7a093c977eec1ebc89328
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-26 12:58:13 +0100

    fix: added getters to class Person

--- src/main/java/exercici2/Person.java
+++ src/main/java/exercici2/Person.java
@@ -14,6 +14,16 @@ public class Person {
         this.age = age;
     }
 
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    
+    public int getAge() {
+        return age;
+    }
     @Override
     public String toString() {
         return "Person{" +
